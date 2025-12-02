6d109b0e0a41bcd106d5f8bd090c7e34fa792589
Rafaela Lemes <r.lemesdemorais@gmail.com>
2025-12-02 13:01:26 -0300

    :sparkles: Atualização Deploy

--- src/services/categoriaService.ts
+++ src/services/categoriaService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { Categoria } from "../models/Categoria";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://farmacia-ug0p.onrender.com/";
 
 export const api = axios.create({
     baseURL: BASE_URL,
